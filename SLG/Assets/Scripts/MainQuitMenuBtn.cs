fd1a07641ba9a2d7b636f224f7680232cef040cd
jhq123 <46354179+jhq123@users.noreply.github.com>
2019-02-26 13:49:50 -0800

    Basic UI applied

---
+++ SLG/Assets/Scripts/MainQuitMenuBtn.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class MainQuitMenuBtn : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Button btn = this.GetComponent<Button>();
+        btn.onClick.AddListener(OnClick);
+    }
+
+    // Update is called once per frame
+
+    void OnClick()
+    {
+        SceneManager.LoadScene("begin");
+    }
+}
