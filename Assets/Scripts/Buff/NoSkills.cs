72bdd95b9664f876d726c92ad57dac003e174b2b
Jianing Xu <jianingx@usc.edu>
2019-04-11 12:53:21 -0700

    version 4_11_2019_1
    1. 重做了Buff模型版->
    1.1 增加了unApply() 功能用于处理Buff在外界取消结算时buff依然存在的问题
    1.2 增加了buff title 的说明，可以直接获取title信息
    2. 更新了unitAttirbute中的东西
    2.1 unitAttirbute中添加用于删除Buff的removeBuffable的东西

---
+++ Assets/Scripts/Buff/NoSkills.cs
@@ -0,0 +1,73 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NoSkills : Buff
+{
+    private int recentTurn = 0;
+
+    private string description = "Cou shu! ";
+
+    private string buffName = "NoSkills";
+
+    public string BuffName
+    {
+        set
+        {
+
+        }
+        get
+        {
+            return this.buffName;
+        }
+    }
+
+    public string Description
+    {
+        set
+        {
+
+        }
+        get
+        {
+            return this.description;
+        }
+    }
+
+    public void Apply(Component charUnit)
+    {
+        
+    }
+
+    public void UnApply()
+    {
+
+    }
+
+    public bool FinishTurn
+    //用于判断是否buff效果结束
+    //因为是被动技能效果，所以始终不会结束（始终return false）
+    {
+        set
+        {
+
+        }
+        get
+        {
+            return false;
+        }
+    }
+
+    public int RecentTurn
+    {
+        //用于获取游戏当前回合数
+        set
+        {
+            this.recentTurn = value;
+        }
+        get
+        {
+            return this.recentTurn;
+        }
+    }
+}
---
+++ Assets/Scripts/UnitAttribute.cs
@@ -0,0 +1,138 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+public class UnitAttribute : MonoBehaviour
+{
+    private int id;//chessID
+
+    public string actorName;//Actor Name
+
+
+    public int hp = 100;//HealthPower
+
+    public int hpMin = 0;//用于检测不死效果 -> if 1
+
+    public int hpMax = 100;//HealthPowerMax
+
+
+    public int ap = 24;//ActionPower
+
+    public int apTemp = 0;//附加行动力
+
+    public int Ap
+    {
+        get
+        {
+            return ap + apTemp;
+        }
+    }
+
+    public int att = 20;//AttackDamage
+
+    public int attTemp = 0;//附加攻击力
+
+    public int Att
+    {
+        get
+        {
+            return att + attTemp;
+        }
+    }
+
+    public int def = 0;//Defense
+
+    public int defTemp = 0;//附加攻击力
+
+    public int Def
+    {
+        get
+        {
+            return def + defTemp;
+        }
+    }
+
+    public int sp = 0;//speed
+
+    public int spTemp = 0;//附加速度
+
+    public int Sp
+    {
+        get
+        {
+            return sp + spTemp;
+        }
+    }
+
+
+    public behaviorStatus bs;//棋子状态
+
+    public int team;//棋子队伍 0->己方  1->敌方  2->中立
+
+    public List<Buff> Buffables = new List<Buff>(); //所有buff
+
+    public int passSkillNumber;//技能编号
+
+    public Buff passSkill;//被动技能
+
+    public int range = 1;//攻击范围 -> 默认为1，暂不可用
+
+
+
+    public void AddBuffable(Buff ibuff)
+    {
+        Debug.Log(Buffables.Count);
+        Buffables.Add(ibuff);
+    }
+
+    public void RemoveBuffable(Buff ibuff)
+    {
+        var result = from r in Buffables
+                     group r by (r.GetType() == ibuff.GetType()) into g
+                     where g.Count() > 1
+                     select g;
+        foreach(var a in result)
+        {
+            foreach(Buff buff in a)
+            {
+                buff.UnApply();
+                Debug.Log("RemoveBuff: " + Buffables.Remove(buff));
+            }
+            
+        }
+        Debug.Log("RestBuff = " + Buffables.Count);
+    }
+
+    //public int checkIndex(Buff ibuff)
+    //{
+    //    return Buffables.IndexOf(ibuff);
+    //}
+
+    public bool checkBuffable(Buff ibuff)
+    {
+        return Buffables.Contains(ibuff);
+    }
+
+    public void Update()
+    {
+        //buff结算
+        for (int i = 0; i < Buffables.Count; i++)
+        {
+            Buffables[i].Apply(this);
+
+            if (Buffables[i].FinishTurn)
+                Buffables.Remove(Buffables[i]);
+        }
+        //如果buff池已加载成功且当前角色拥有被动技能
+        if (passSkill!=null)
+        {
+            //被动技能结算
+            passSkill.Apply(this);
+        }
+        else if (passSkill==null)
+        {
+            passSkill = BuffPool.getPassiveSkills(this.passSkillNumber);
+        }
+    }
+}
